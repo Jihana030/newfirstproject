d9b6a3663d4225d6a669dde2656ee4d2a2454a17
BaePippi <119798002+BaePippi@users.noreply.github.com>
2022-12-27 12:41:49 +0900

    Delete js directory

--- js/mouseCurser.js
+++
@@ -1,8 +0,0 @@
-let mouseCursor = document.getElementById("cursor");
-console.log(mouseCursor)
-
-document.addEventListener("mousemove", cursor);
-function cursor(e) {
-  mouseCursor.style.top = e.pageY + document.body.scrollTop + 15 + "px";
-  mouseCursor.style.left = e.pageX + document.body.scrollLeft+ 15 + "px";
-}
